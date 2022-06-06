a7f4111e8a8b5c469871f71ecfcce6609777a79d
Marc Vilella <mvilella.and@gmail.com>
2022-06-06 22:44:08 +0200

    refactor: Extract URL builder to own module

--- src/airtable.js
+++ src/airtable.js
@@ -1,5 +1,5 @@
 import HttpError from './error.http';
-import createFormula from './formula';
+import useURL from './use.url';
 import { flattenRecord, useRecords } from './use.records';
 
 const BASE_URL = 'https://api.airtable.com/v0';
@@ -9,31 +9,7 @@ export default ({ base, token, baseURL = BASE_URL } = {}) => {
 	if (!token) throw new Error('Airtable API token is required');
 	const CONFIG = { headers: { Authorization: `Bearer ${token}` } };
 
-	const buildURL = (resource, options = {}) => {
-		if (!resource) throw new Error('Airtable resource is required');
-		const { base: overrideBase, fields = [], sort = {}, max, view, offset, where } = options;
-		const url = new URL(`${baseURL}/${overrideBase || base}/${resource}`);
-
-		// Add direct parameters (if required)
-		if (view) url.searchParams.append('view', view);
-		if (max) url.searchParams.append('maxRecords', max);
-		if (offset) url.searchParams.append('offset', offset);
-
-		// Serialize fields[] option
-		fields.forEach(field => url.searchParams.append('fields[]', field));
-
-		// Serialize sort options
-		Object.entries(sort).forEach(([field, order], i) => {
-			url.searchParams.append(`sort[${i}][field]`, field);
-			url.searchParams.append(`sort[${i}][direction]`, order);
-		});
-
-		// Build filter formula
-		const formula = createFormula(where);
-		if (formula) url.searchParams.append('filterByFormula', formula);
-
-		return url;
-	};
+	const buildURL = useURL({ baseURL, base });
 
 	const query = async (resource, options = {}) => {
 		if (!resource) throw new Error('Airtable resource is required');
---
+++ src/use.url.js
@@ -0,0 +1,27 @@
+import createFormula from './formula';
+
+export default ({ baseURL, base }) => (resource, options = {}) => {
+	if (!resource) throw new Error('Airtable resource is required');
+	const { base: overrideBase, fields = [], sort = {}, max, view, offset, where } = options;
+	const url = new URL(`${baseURL}/${overrideBase || base}/${resource}`);
+
+	// Add direct parameters (if required)
+	if (view) url.searchParams.append('view', view);
+	if (max) url.searchParams.append('maxRecords', max);
+	if (offset) url.searchParams.append('offset', offset);
+
+	// Serialize fields[] option
+	fields.forEach(field => url.searchParams.append('fields[]', field));
+
+	// Serialize sort options
+	Object.entries(sort).forEach(([field, order], i) => {
+		url.searchParams.append(`sort[${i}][field]`, field);
+		url.searchParams.append(`sort[${i}][direction]`, order);
+	});
+
+	// Build filter formula
+	const formula = createFormula(where);
+	if (formula) url.searchParams.append('filterByFormula', formula);
+
+	return url;
+};
