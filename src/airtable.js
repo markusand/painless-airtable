1a527c922556c5d31db763089ead398ecbaebc6b
Marc Vilella <mvilella.and@gmail.com>
2022-06-06 22:43:37 +0200

    refactor: Extract records manipulation to own module

--- src/airtable.js
+++ src/airtable.js
@@ -1,5 +1,6 @@
 import HttpError from './error.http';
 import createFormula from './formula';
+import { flattenRecord, useRecords } from './use.records';
 
 const BASE_URL = 'https://api.airtable.com/v0';
 
@@ -7,44 +8,6 @@ export default ({ base, token, baseURL = BASE_URL } = {}) => {
 	if (!base) throw new Error('Airtable base is required');
 	if (!token) throw new Error('Airtable API token is required');
 	const CONFIG = { headers: { Authorization: `Bearer ${token}` } };
-
-	const flattenRecord = record => ({
-		_id: record.id,
-		_created: record.createdTime,
-		...record.fields,
-	});
-
-	const flattenRecords = (records, index) => (
-		index
-			? records.reduce((acc, record) => ({ ...acc, [record.id]: flattenRecord(record) }), {})
-			: records.map(flattenRecord)
-	);
-
-	const expandRecords = async (records, expand) => {
-		if (!expand) return records;
-		// Recollect all unique record IDs for each field and query them to the API
-		const queries = Object.entries(expand).map(async ([field, { table, options }]) => {
-			const ids = records.flatMap(record => record.fields[field]);
-			const unique = [...new Set(ids)].map(id => `RECORD_ID()='${id}'`).join(',');
-			const url = `${table}?filterByFormula=OR(${unique})`;
-			// eslint-disable-next-line no-use-before-define
-			const expanded = await select(url, { ...options, index: true });
-			return { field, expanded };
-		});
-		// Wait for all queries to complete
-		const expandable = await Promise.all(queries);
-		// Replace expandable IDs with their expanded records
-		return records.map(({ fields, ...rest }) => ({
-			...rest,
-			fields: {
-				...fields,
-				...expandable.reduce((acc, { field, expanded }) => {
-					if (fields[field]) acc[field] = fields[field].map(id => expanded[id]);
-					return acc;
-				}, {}),
-			},
-		}));
-	};
 
 	const buildURL = (resource, options = {}) => {
 		if (!resource) throw new Error('Airtable resource is required');
@@ -83,14 +46,13 @@ export default ({ base, token, baseURL = BASE_URL } = {}) => {
 
 	const select = async (table, options = {}) => {
 		if (!table) throw new Error('Airtable table is required');
-		const { index, persist, expand, flatten = true, ...rest } = options;
-		const { records = [], offset } = await query(table, options);
-		const more = offset && persist
-			? await select(table, { ...rest, persist, flatten: false, offset })
-			: [];
-		const all = [...records, ...more];
-		const expanded = await expandRecords(all, expand);
-		return flatten ? flattenRecords(expanded, index) : expanded;
+		const response = await query(table, options);
+		const records = useRecords(response.records, select, options);
+		// Each method determines internally if action must be performed
+		await records.persist(table, response.offset);
+		await records.expand();
+		records.flatten();
+		return records.getAll();
 	};
 
 	const find = async (table, id, options = {}) => {
---
+++ src/use.records.js
@@ -0,0 +1,51 @@
+export const flattenRecord = record => ({
+	_id: record.id,
+	_created: record.createdTime,
+	...record.fields,
+});
+
+export const useRecords = (dirtyRecords = [], select, options) => {
+	let records = dirtyRecords;
+
+	const flatten = () => {
+		// Flatten when options.flatten is true, undefined or null
+		if (!(options.flatten ?? true)) return;
+		records = options.index
+			? records.reduce((acc, record) => ({ ...acc, [record.id]: flattenRecord(record) }), {})
+			: records.map(flattenRecord);
+	};
+
+	const persist = async (table, offset) => {
+		if (!options.persist || !offset) return;
+		const forceRaw = { flatten: false, expand: false, index: false };
+		const more = await select(table, { ...options, offset, ...forceRaw });
+		records = [...records, ...more];
+	};
+
+	const expand = async () => {
+		if (!options.expand) return;
+		// Recollect all unique record IDs for each field and query them to the API
+		const queries = Object.entries(options.expand).map(async ([field, expander]) => {
+			const ids = records.flatMap(record => record.fields[field]);
+			const unique = [...new Set(ids)].map(id => `RECORD_ID()='${id}'`).join(',');
+			const url = `${expander.table}?filterByFormula=OR(${unique})`;
+			// eslint-disable-next-line no-use-before-define
+			const expanded = await select(url, { ...expander.options, index: true });
+			return { field, expanded };
+		});
+		// Wait for all queries to complete
+		const expandable = await Promise.all(queries);
+		// Replace expandable IDs with their expanded records
+		records = records.map(({ fields, ...rest }) => {
+			const expandedFields = expandable.reduce((acc, { field, expanded }) => {
+				if (fields[field]) acc[field] = fields[field].map(id => expanded[id]);
+				return acc;
+			}, {});
+			return { ...rest, fields: { ...fields, ...expandedFields } };
+		});
+	};
+
+	const getAll = () => records;
+
+	return { flattenRecord, flatten, expand, persist, getAll };
+};
