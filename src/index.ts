2467c406cb8faf2d80089f6706f9dd360e2d2baa
Marc Vilella <mvilella.and@gmail.com>
2023-04-04 19:06:15 +0200

    feat: Export HttpError

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as useAirtable } from './use.airtable';
 export { default as createFormula } from './formula';
+export { default as HttpError } from './error.http';
 
 export * from './types';
